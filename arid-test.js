bea8a9339b02f9fffaef43a820e9cef75deead07
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-04 16:17:24 +0800

    Updated server-side test.

--- arid-test.js
+++ arid-test.js
@@ -1,8 +1,8 @@
 const assert = require( "assert" );
 const arid = require( "./arid.js" );
 
-assert.equal( arid( [  ] ), true, "should be true" );
-assert.equal( arid( [ { } ] ), false, "should be false" );
+assert.equal( arid( [ ] ), true, "should be true" );
+
 assert.equal( arid( [ 1, 2, 3 ] ), false, "should be false" );
 
 console.log( "ok" );
