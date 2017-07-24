16bda0d10fc57790781695e047af352c8c8f8d3e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-24 16:08:59 +0800

    Updated test.

--- arid-test.js
+++ arid-test.js
@@ -1,8 +1,8 @@
 const assert = require( "assert" );
 const arid = require( "./arid.js" );
 
-assert.equal( arid( [ ] ), true, "should be true" );
+assert.equal( arid( [ ] ), true, "should return true" );
 
-assert.equal( arid( [ 1, 2, 3 ] ), false, "should be false" );
+assert.equal( arid( [ 1, 2, 3 ] ), false, "should return false" );
 
 console.log( "ok" );
