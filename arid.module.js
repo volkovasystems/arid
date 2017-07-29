0285b180813e6b279dab5e56774825ea7a46840e
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-07-30 02:10:22 +0800

    Update build procedure.

---
+++ arid.module.js
@@ -0,0 +1,75 @@
+"use strict";
+
+/*;
+	@module-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"package": "arid",
+			"path": "arid/arid.js",
+			"file": "arid.js",
+			"module": "arid",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/arid.git",
+			"test": "arid-test.js",
+			"global": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		Check if array is absolutely empty.
+
+		Array containing falsy values considered absolute empty.
+	@end-module-documentation
+
+	@include:
+		{
+			"arkount": "arkount"
+		}
+	@end-include
+*/
+
+const arkount = require( "arkount" );
+
+const arid = function arid( array ){
+	/*;
+		@meta-configuration:
+			{
+				"array:required": "[*]"
+			}
+		@end-meta-configuration
+	*/
+
+	return ( arkount( array ) == 0 );
+};
+
+module.exports = arid;
