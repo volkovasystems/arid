6b192f35c0f0d138f8ce634d2c619bfab996cf4c
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-07-31 13:28:37 +0800

    Update client/server unit test.

--- test.module..js
+++ test.module..js
@@ -1,8 +1,135 @@
+"use strict";
+
+/*;
+	@test-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-test-license
+
+	@test-configuration:
+		{
+			"package": "arid",
+			"path": "arid/test.module.js",
+			"file": "test.module.js",
+			"module": "test",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/arid.git"
+		}
+	@end-test-configuration
+
+	@test-documentation:
+
+	@end-test-documentation
+
+	@include:
+		{
+			"assert": "should",
+			"arid": "arid"
+		}
+	@end-include
+*/
+
 const assert = require( "assert" );
+
+//: @server:
 const arid = require( "./arid.js" );
+//: @end-server
 
-assert.equal( arid( [ ] ), true, "should return true" );
+//: @client:
+const arid = require( "./arid.support.js" );
+//: @end-client
 
-assert.equal( arid( [ 1, 2, 3 ] ), false, "should return false" );
+//: @bridge:
+const path = require( "path" );
+//: @end-bridge
 
-console.log( "ok" );
+//: @server:
+describe( "arid", ( ) => {
+	
+	describe( `"arid( [ ] )"`, ( ) => {
+		it( "should return true", ( ) => {
+
+			assert.equal( arid( [ ] ), true );
+
+		} );
+	} );
+
+	describe( `"arid( [ 1, 2, 3 ] )"`, ( ) => {
+	it( "should return false", ( ) => {
+
+			assert.equal( arid( [ 1, 2, 3 ] ), false);
+
+		} );
+	} );
+
+	
+} );
+
+
+//: @end-server
+
+
+//: @client: 
+
+describe( "arid", ( ) => {
+	
+	describe( `"arid( [ ] )"`, ( ) => {
+		it( "should return true", ( ) => {
+
+			assert.equal( arid( [ ] ), true );
+
+		} );
+	} );
+
+	describe( `"arid( [ 1, 2, 3 ] )"`, ( ) => {
+	it( "should return false", ( ) => {
+
+			assert.equal( arid( [ 1, 2, 3 ] ), false);
+
+		} );
+	} );
+
+	
+} );
+
+//: @end-client
+
+
+//: @bridge:
+
+
+
+//: @end-bridge
+
+
+// assert.equal( arid( [ ] ), true, "should return true" );
+
+// assert.equal( arid( [ 1, 2, 3 ] ), false, "should return false" );
+
+// console.log( "ok" );
